cb717b269e5750f9fa55ddba96e5c600ccb50fad
seanclynes <sean.clynes@gmail.com>
2016-08-12 13:54:55 +1000

    most basic tests for simplest functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,12 +23,6 @@ function toggleString(toBRemoved, toBAdded, str) {
     return str.replace(toBRemoved, '').trim() + ' ' + toBAdded;
 }
 
-
-/**
- * Google maps API specific functions
- * */
-
-
 function callLatLngGetFunctions(propertyName, object) {
     'use strict';
 
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,7 +1,44 @@
 describe('charFromStr', function () {
 
     it('should return first char from a string', function () {
-        //expect(greet('World')).toEqual('Hello, World!');
-        expect(1 + 2).toEqual(3);
+        var str = 'abc';
+        expect(charFromStr(0, str)).toEqual('a');
+    });
+
+    it('should return first char for index greater then str length', function() {
+        var str = 'abc';
+        expect(charFromStr(3, str)).toEqual('a');
+    });
+});
+
+describe('convertTo360Heading', function() {
+
+    it('should convert -90 to 270', function() {
+        expect(convertTo360Heading(-90)).toEqual(270);
+    });
+
+    it('should return 100 as 100', function() {
+        expect(convertTo360Heading(100)).toEqual(100);
+    });
+
+    it('should round 45.8 to 46', function() {
+        expect(convertTo360Heading(45.8)).toEqual(46);
+    });
+});
+
+describe('toggleString', function() {
+
+    it('should replace aaa with bbb', function() {
+        var str = 'aaa is replaced';
+
+        expect(toggleString('aaa','bbb', str)).toEqual('is replaced bbb');
+    });
+
+    it('should replace aaa with bbb with Idempotence', function() {
+        var str = 'aaa is replaced', replaced;
+
+        replaced = toggleString('aaa','bbb', str);
+        replaced = toggleString('bbb','aaa', replaced);
+        expect(replaced.indexOf('aaa')).toBeGreaterThan(-1);
     });
 });
